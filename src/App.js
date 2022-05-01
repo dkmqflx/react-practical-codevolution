496da7b57089c57cb40efa3d467be71cdc38302e
dkmqflx <dkmqflx@gmail.com>
2022-05-01 16:18:15 +0900

    2. Icons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { FaReact } from 'react-icons/fa';
+import { MdAlarmAdd } from 'react-icons/md';
+import { IconContext } from 'react-icons';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <IconContext.Provider value={{ color: 'blue', size: '5rem' }}>
+      <div className="App">
+        {/* <FaReact color="purple" size="10rem"></FaReact>
+        <MdAlarmAdd color="purple" size="10rem"></MdAlarmAdd> */}
+        <FaReact></FaReact>
+        <MdAlarmAdd></MdAlarmAdd>
+      </div>
+    </IconContext.Provider>
   );
 }
+
+// IconContext.Provider로 style 속성을 한번에 지정해줄 수 있다.
+// 직접 아이콘에 선언해서 overrid할 수 도 있다.
 
 export default App;
