5323ed0f8949de3a0090d953af810b6baf629e75
dkmqflx <dkmqflx@gmail.com>
2022-05-01 17:37:58 +0900

    6. CountUp

--- src/App.js
+++ src/App.js
@@ -1,48 +1,44 @@
 import './App.css';
-import Tippy from '@tippy.js/react';
-import 'tippy.js/dist/tippy.css';
-import { forwardRef } from 'react';
-
-const ColoredTooltip = () => {
-  return <span style={{ color: 'yellow' }}>Colored Component</span>;
-};
-
-// customize component 사용하기 위해서는 forwardRef 사용해야 한다.
-const CustomChild = forwardRef((props, ref) => {
-  return (
-    <div ref={ref}>
-      <div>First line</div>
-      <div>Second line</div>
-    </div>
-  );
-});
+import CountUp, { useCountUp } from 'react-countup';
+import { useRef } from 'react';
 
 function App() {
+  const countUpRef = useRef();
+  const { countUp, start, pauseResume, reset, update } = useCountUp({
+    ref: countUpRef,
+    duration: 5,
+    end: 10000,
+    startOnMount: false,
+  });
   return (
     <div className="App">
-      <div style={{ paddingBottom: '20px' }}>
-        <Tippy placement="right" arrow={false} delay={1000} content="Basic Tooltip">
-          <button>Hover</button>
-        </Tippy>
-      </div>
+      <dir>
+        <div ref={countUpRef}></div>
+        <h1>{countUp}</h1>
+        <button onClick={start}>Start</button>
+        <button onClick={reset}>reset</button>
+        <button onClick={pauseResume}>pause / resume</button>
+        <button onClick={() => update(2000)}>update to 2000</button>
+      </dir>
 
-      <div style={{ paddingBottom: '20px' }}>
-        <Tippy content={<span style={{ color: 'orange' }}> Colered</span>}>
-          <button>Hover</button>
-        </Tippy>
-      </div>
-
-      <div style={{ paddingBottom: '20px' }}>
-        <Tippy content={<ColoredTooltip></ColoredTooltip>}>
-          <button>Hover</button>
-        </Tippy>
-      </div>
-
-      <div style={{ paddingBottom: '20px' }}>
-        <Tippy placement="top-start" content={<ColoredTooltip></ColoredTooltip>}>
-          <CustomChild></CustomChild>
-        </Tippy>
-      </div>
+      <h1>
+        <CountUp end={200} />
+      </h1>
+      <br />
+      <h1>
+        <CountUp end={200} duration={5} />
+      </h1>
+      <br />
+      <h1>
+        <CountUp start={500} end={1000} duration={5} />
+      </h1>
+      <br />
+      <h1>
+        <CountUp end={1000} duration={5} prefix="$" decimals={2} />
+      </h1>
+      <h1>
+        <CountUp end={1000} duration={5} suffix="USD" decimals={2} />
+      </h1>
     </div>
   );
 }
